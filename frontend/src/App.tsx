464d19c6f62dea7ce66c5bcd3ec54b6d040d1cff
Eric Xiao <ericxiao0000@gmail.com>
2022-05-23 19:50:53 -0700

    FRONTEND: Chat box now scrollable, auto scrolls when new message sent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
+import { useEffect } from "react";
 import { ChangeEvent, FormEvent, useState } from "react";
 
 const App = () => {
-  const [chatMessages, setChatMessages] = useState<Array<String>>(["woah"]);
+  const [messages, setMessages] = useState<Array<String>>([]);
 
   const [curMessage, setCurMessage] = useState<string>("");
 
+  const handleNewMessage = () => {
+    let chatBox = document.getElementById("chat-box");
+    if (chatBox !== null) {
+      chatBox.scrollTop = chatBox.scrollHeight;
+    }
+  };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    let newMessages = [...chatMessages];
+    let newMessages = [...messages];
     newMessages.push(curMessage);
-    setChatMessages(newMessages);
+    setMessages(newMessages);
     setCurMessage("");
+    handleNewMessage();
   };
 
   const handleMessageChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -18,22 +27,21 @@ const App = () => {
     setCurMessage(event.target.value);
   };
 
+  useEffect(() => {
+    handleNewMessage();
+  }, [messages]);
+
   return (
-    <div className="app">
-      <h1 className="app-title">Global Chat</h1>
-      <div className="chat-box">
-        {chatMessages.map((msg, index) => {
+    <div id="app">
+      <h1 id="app-title">Global Chat</h1>
+      <div id="chat-box">
+        {messages.map((msg, index) => {
           return <p key={index}>{msg}</p>;
         })}
       </div>
-      <form className="chat-input" onSubmit={handleSubmit}>
+      <form id="chat-input" onSubmit={handleSubmit}>
         <label>
-          <input
-            type="text"
-            name="name"
-            value={curMessage}
-            onChange={handleMessageChange}
-          />
+          <input value={curMessage} onChange={handleMessageChange} />
         </label>
 
         <input type="submit" value="Send" />
